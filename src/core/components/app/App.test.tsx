c08cde9ca06c1c1b9b519404732f752b8c417ad5
xistri <cristian93marti@gmail.com>
2022-12-15 13:49:24 +0100

    test app and fix package json

---
+++ src/core/components/app/App.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('Given App component', () => {
+    test('renders tittle', () => {
+        render(<App />);
+        const element = screen.getByText(/The pointing gentlemen/i);
+        expect(element).toBeInTheDocument();
+    });
+});
